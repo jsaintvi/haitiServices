be23c5d9b3dfd50053eba0e21f1a1a1038b267b3
Jodex Saint-Vil <jsaintvi@aol.com>
2015-10-02 18:52:19 -0400

    Generated places scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
 
   validates_presence_of :name
+
+  has_many :places
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :places
   devise_for :users, controllers: {registrations: "registrations"}
   root 'pages#home'
 
