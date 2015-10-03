f25d718b7d47872093a7b0ecd07a3713d30d8a79
Jodex Saint-Vil <jsaintvi@aol.com>
2015-10-03 02:13:09 -0400

    fixing routes and improved landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :places
   devise_for :users, controllers: {registrations: "registrations"}
   root 'pages#home'
+  get '/places' => 'places#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
